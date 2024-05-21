072361060c6b0eab3a2753dc028955272a37004d
Ronit Rangwani <ronitrangwani@gmail.com>
2024-05-22 00:36:07 +0530

    integrated stripe webhook

--- src/libs/apis.ts
+++ src/libs/apis.ts
@@ -1,12 +1,10 @@
-// import { CreateReviewDto, Review } from './../models/review';
-// import axios from 'axios';
-
+import { CreateReviewDto, Review } from './../models/review';
 import { CreateBookingDto, Room } from '@/models/room';
 import sanityClient from './sanity';
 import * as queries from './sanityQueries';
 import axios from 'axios';
-// import { Booking } from '@/models/booking';
-// import { UpdateReviewDto } from '@/models/review';
+import { Booking } from '@/models/booking';
+import { UpdateReviewDto } from '@/models/review';
 
 export async function getFeaturedRoom() {
   const result = await sanityClient.fetch<Room>(
@@ -76,140 +74,140 @@ export const createBooking = async ({
   return data;
 };
 
-// export const updateHotelRoom = async (hotelRoomId: string) => {
-//   const mutation = {
-//     mutations: [
-//       {
-//         patch: {
-//           id: hotelRoomId,
-//           set: {
-//             isBooked: true,
-//           },
-//         },
-//       },
-//     ],
-//   };
+export const updateHotelRoom = async (hotelRoomId: string) => {
+  const mutation = {
+    mutations: [
+      {
+        patch: {
+          id: hotelRoomId,
+          set: {
+            isBooked: true,
+          },
+        },
+      },
+    ],
+  };
 
-//   const { data } = await axios.post(
-//     `https://${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}.api.sanity.io/v2021-10-21/data/mutate/${process.env.NEXT_PUBLIC_SANITY_DATASET}`,
-//     mutation,
-//     { headers: { Authorization: `Bearer ${process.env.SANITY_STUDIO_TOKEN}` } }
-//   );
+  const { data } = await axios.post(
+    `https://${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}.api.sanity.io/v2021-10-21/data/mutate/${process.env.NEXT_PUBLIC_SANITY_DATASET}`,
+    mutation,
+    { headers: { Authorization: `Bearer ${process.env.SANITY_STUDIO_TOKEN}` } }
+  );
 
-//   return data;
-// };
+  return data;
+};
 
-// export async function getUserBookings(userId: string) {
-//   const result = await sanityClient.fetch<Booking[]>(
-//     queries.getUserBookingsQuery,
-//     {
-//       userId,
-//     },
-//     { cache: 'no-cache' }
-//   );
+export async function getUserBookings(userId: string) {
+  const result = await sanityClient.fetch<Booking[]>(
+    queries.getUserBookingsQuery,
+    {
+      userId,
+    },
+    { cache: 'no-cache' }
+  );
 
-//   return result;
-// }
+  return result;
+}
 
-// export async function getUserData(userId: string) {
-//   const result = await sanityClient.fetch(
-//     queries.getUserDataQuery,
-//     { userId },
-//     { cache: 'no-cache' }
-//   );
+export async function getUserData(userId: string) {
+  const result = await sanityClient.fetch(
+    queries.getUserDataQuery,
+    { userId },
+    { cache: 'no-cache' }
+  );
 
-//   return result;
-// }
+  return result;
+}
 
-// export async function checkReviewExists(
-//   userId: string,
-//   hotelRoomId: string
-// ): Promise<null | { _id: string }> {
-//   const query = `*[_type == 'review' && user._ref == $userId && hotelRoom._ref == $hotelRoomId][0] {
-//     _id
-//   }`;
+export async function checkReviewExists(
+  userId: string,
+  hotelRoomId: string
+): Promise<null | { _id: string }> {
+  const query = `*[_type == 'review' && user._ref == $userId && hotelRoom._ref == $hotelRoomId][0] {
+    _id
+  }`;
 
-//   const params = {
-//     userId,
-//     hotelRoomId,
-//   };
+  const params = {
+    userId,
+    hotelRoomId,
+  };
 
-//   const result = await sanityClient.fetch(query, params);
+  const result = await sanityClient.fetch(query, params);
 
-//   return result ? result : null;
-// }
+  return result ? result : null;
+}
 
-// export const updateReview = async ({
-//   reviewId,
-//   reviewText,
-//   userRating,
-// }: UpdateReviewDto) => {
-//   const mutation = {
-//     mutations: [
-//       {
-//         patch: {
-//           id: reviewId,
-//           set: {
-//             text: reviewText,
-//             userRating,
-//           },
-//         },
-//       },
-//     ],
-//   };
+export const updateReview = async ({
+  reviewId,
+  reviewText,
+  userRating,
+}: UpdateReviewDto) => {
+  const mutation = {
+    mutations: [
+      {
+        patch: {
+          id: reviewId,
+          set: {
+            text: reviewText,
+            userRating,
+          },
+        },
+      },
+    ],
+  };
 
-//   const { data } = await axios.post(
-//     `https://${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}.api.sanity.io/v2021-10-21/data/mutate/${process.env.NEXT_PUBLIC_SANITY_DATASET}`,
-//     mutation,
-//     { headers: { Authorization: `Bearer ${process.env.SANITY_STUDIO_TOKEN}` } }
-//   );
+  const { data } = await axios.post(
+    `https://${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}.api.sanity.io/v2021-10-21/data/mutate/${process.env.NEXT_PUBLIC_SANITY_DATASET}`,
+    mutation,
+    { headers: { Authorization: `Bearer ${process.env.SANITY_STUDIO_TOKEN}` } }
+  );
 
-//   return data;
-// };
+  return data;
+};
 
-// export const createReview = async ({
-//   hotelRoomId,
-//   reviewText,
-//   userId,
-//   userRating,
-// }: CreateReviewDto) => {
-//   const mutation = {
-//     mutations: [
-//       {
-//         create: {
-//           _type: 'review',
-//           user: {
-//             _type: 'reference',
-//             _ref: userId,
-//           },
-//           hotelRoom: {
-//             _type: 'reference',
-//             _ref: hotelRoomId,
-//           },
-//           userRating,
-//           text: reviewText,
-//         },
-//       },
-//     ],
-//   };
+export const createReview = async ({
+  hotelRoomId,
+  reviewText,
+  userId,
+  userRating,
+}: CreateReviewDto) => {
+  const mutation = {
+    mutations: [
+      {
+        create: {
+          _type: 'review',
+          user: {
+            _type: 'reference',
+            _ref: userId,
+          },
+          hotelRoom: {
+            _type: 'reference',
+            _ref: hotelRoomId,
+          },
+          userRating,
+          text: reviewText,
+        },
+      },
+    ],
+  };
 
-//   const { data } = await axios.post(
-//     `https://${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}.api.sanity.io/v2021-10-21/data/mutate/${process.env.NEXT_PUBLIC_SANITY_DATASET}`,
-//     mutation,
-//     { headers: { Authorization: `Bearer ${process.env.SANITY_STUDIO_TOKEN}` } }
-//   );
+  const { data } = await axios.post(
+    `https://${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}.api.sanity.io/v2021-10-21/data/mutate/${process.env.NEXT_PUBLIC_SANITY_DATASET}`,
+    mutation,
+    { headers: { Authorization: `Bearer ${process.env.SANITY_STUDIO_TOKEN}` } }
+  );
 
-//   return data;
-// };
+  return data;
+};
 
-// export async function getRoomReviews(roomId: string) {
-//   const result = await sanityClient.fetch<Review[]>(
-//     queries.getRoomReviewsQuery,
-//     {
-//       roomId,
-//     },
-//     { cache: 'no-cache' }
-//   );
+export async function getRoomReviews(roomId: string) {
+  const result = await sanityClient.fetch<Review[]>(
+    queries.getRoomReviewsQuery,
+    {
+      roomId,
+    },
+    { cache: 'no-cache' }
+  );
 
-//   return result;
-// }
+  return result;
+}
